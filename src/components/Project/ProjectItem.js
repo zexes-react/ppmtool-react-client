7dc61c9394f09fd7a4640ff516acfe3ccc08d4d2
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-11-21 21:07:27 +0100

    Project and header components. closes #2

---
+++ src/components/Project/ProjectItem.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import Aux from '../../hoc/Auxilliary';
+
+class ProjectItem extends Component {
+    render() {
+        return (
+            <Aux>
+                <h1>Project Item</h1>
+            </Aux>
+        );
+    }
+}
+
+export default ProjectItem;
